49f7ba8216d80b31107c95fba90410378739267c
Phil Bredeson <phil.bredeson@gmail.com>
2021-04-14 18:42:12 -0600

    added dist

---
+++ dist/index.d.ts
@@ -0,0 +1,20 @@
+/****************************************************
+ Main
+****************************************************/
+export declare function EntityQuery<T extends {
+    [key: string]: R;
+}, R extends Obj>(entities: T): {
+    filter(query?: Query, options?: QueryOptions): R[];
+    search(query?: Query, options?: QueryOptions): string[];
+};
+declare type Obj = {
+    id: string;
+    [key: string]: any;
+};
+export declare type Query = {
+    [key: string]: any;
+};
+declare type QueryOptions = {
+    conditions?: "all" | "any" | "diff" | "none";
+};
+export {};
