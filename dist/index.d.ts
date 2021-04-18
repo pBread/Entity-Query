d848bfa51ab98baaeee18697f905c6647177fae8
Phil Bredeson <phil.bredeson@gmail.com>
2021-04-18 12:36:53 -0600

    npm build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,20 +1,20 @@
 /****************************************************
  Main
 ****************************************************/
-declare function EntityQuery<T extends {
+export default function EntityQuery<T extends {
     [key: string]: R;
 }, R extends Obj>(entities: T): {
     filter(query?: Query, options?: QueryOptions): R[];
     search(query?: Query, options?: QueryOptions): string[];
 };
-export default EntityQuery;
+export declare type Query = {
+    [key: string]: any;
+};
+export declare type QueryOptions = {
+    conditions?: "all" | "any" | "diff" | "none";
+};
 declare type Obj = {
     id: string;
     [key: string]: any;
 };
-export declare type Query = {
-    [key: string]: any;
-};
-declare type QueryOptions = {
-    conditions?: "all" | "any" | "diff" | "none";
-};
+export {};
